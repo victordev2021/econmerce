2ae7211071f04a71a554de8d329eb9cb3f917fb8
Victor Avalos Torrez <tigervat10@gmail.com>
2022-02-26 11:37:08 -0400

    Incluir dropdown

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -12,5 +12,75 @@
             <x-jet-application-mark class="block h-9 w-auto" />
         </a>
         @livewire('search')
+        <!-- Settings Dropdown -->
+        <div class="ml-3 relative">
+            @auth
+                <x-jet-dropdown align="right" width="48">
+                    <x-slot name="trigger">
+                        @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
+                            <button
+                                class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
+                                <img class="h-8 w-8 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}"
+                                    alt="{{ Auth::user()->name }}" />
+                            </button>
+                        @else
+                            <span class="inline-flex rounded-md">
+                                <button type="button"
+                                    class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition">
+                                    {{ Auth::user()->name }}
+
+                                    <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+                                        fill="currentColor">
+                                        <path fill-rule="evenodd"
+                                            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                            clip-rule="evenodd" />
+                                    </svg>
+                                </button>
+                            </span>
+                        @endif
+                    </x-slot>
+
+                    <x-slot name="content">
+                        <!-- Account Management -->
+                        <div class="block px-4 py-2 text-xs text-gray-400">
+                            {{ __('Manage Account') }}
+                        </div>
+
+                        <x-jet-dropdown-link href="{{ route('profile.show') }}">
+                            {{ __('Profile') }}
+                        </x-jet-dropdown-link>
+
+                        <div class="border-t border-gray-100"></div>
+
+                        <!-- Authentication -->
+                        <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+
+                            <x-jet-dropdown-link href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                                        this.closest('form').submit();">
+                                {{ __('Log Out') }}
+                            </x-jet-dropdown-link>
+                        </form>
+                    </x-slot>
+                </x-jet-dropdown>
+            @else
+                <x-jet-dropdown align="right" width="48">
+                    <x-slot name="trigger">
+                        <i class="fas fa-user-circle text-white text-3xl cursor-pointer"></i>
+                    </x-slot>
+                    <x-slot name="content">
+                        <!-- Account Management -->
+                        <x-jet-dropdown-link href="{{ route('login') }}">
+                            {{ __('Login') }}
+                        </x-jet-dropdown-link>
+                        <x-jet-dropdown-link href="{{ route('register') }}">
+                            {{ __('Register') }}
+                        </x-jet-dropdown-link>
+
+                        <div class="border-t border-gray-100"></div>
+                    </x-slot>
+                </x-jet-dropdown>
+            @endauth
+        </div>
     </div>
 </header>
--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -1,3 +1,6 @@
-<div class="flex-1">
+<div class="flex-1 relative">
     <x-jet-input type="text" class="w-full" placeholder="Estas buscando algún producto?" />
+    <button class="absolute top-0 right-0 h-full bg-ochre-400 flex items-center justify-center rounded-r-md px-2">
+        <x-search size=35 color="white" />
+    </button>
 </div>
